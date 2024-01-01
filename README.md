f4346559a89a912462d7afaa4f53bc948001a888
saksham <sakshamjain0464@gmail.com>
2024-01-01 15:14:07 +0530

    Project Completed

---
+++ README.md
@@ -0,0 +1,58 @@
+# Textly - Text-to-Speech Converter
+
+Textly is a lightweight web application that utilizes HTML, CSS, and JavaScript to convert text into speech using the browser's built-in Speech Synthesis API. It supports all available speech engines in the browser, providing a seamless and accessible way to convert written content into spoken words.
+
+## Features
+
+- **Cross-Browser Compatibility:** Textly works on major web browsers, ensuring a consistent experience for users across different platforms.
+- **Speech Engine Support:** It leverages the browser's Speech Synthesis API, supporting all available speech engines, giving users the flexibility to choose their preferred voice.
+- **User-Friendly Interface:** The application provides a simple and intuitive user interface, making it easy for users to input text and initiate the text-to-speech conversion.
+- **Responsive Design:** Textly is designed to be responsive, ensuring a smooth experience on various devices, including desktops, tablets, and smartphones.
+
+## Usage
+
+1. Open the Textly application in a web browser.
+2. Enter the desired text in the input field.
+3. Choose the preferred speech engine from the available options.
+4. Click the "Play" button to initiate the text-to-speech conversion.
+5. Adjust the volume and rate settings if necessary.
+
+## Installation
+
+No installation is required for Textly. Simply open the `index.html` file in a web browser to use the application.
+
+## Reference
+
+https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
+
+## Browser Compatibility
+
+Textly is compatible with the following browsers:
+
+- Google Chrome
+- Mozilla Firefox
+- Microsoft Edge
+- Safari
+- Opera
+
+## Contributing
+
+If you'd like to contribute to Textly, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and ensure the project still works as expected.
+4. Create a pull request detailing your changes.
+
+## Acknowledgments
+
+- The Textly project makes use of the browser's Speech Synthesis API.
+
+- Special thanks to the open-source community for their valuable contributions.
+
+## Contact
+
+For any inquiries or issues, please contact [work.saksham@yahoo.com].
+
+
+
---
+++ script.js
@@ -0,0 +1,82 @@
+let inputText = document.querySelector('#input-text');
+let counter = document.querySelector('p');
+
+// Word Counter
+inputText.addEventListener('input', () => {
+    let input = inputText.value;
+    let words = input.split(' ').length - 1;
+    if(words <= 500){
+        counter.textContent = `${words}/500`;
+    }
+    else{
+        alert("Do not enter more than 500 words!!");
+    }
+    if(!input){
+        counter.textContent = '0/500';
+    }
+})
+
+let speech = new SpeechSynthesisUtterance();
+speech.value = 1
+speech.rate = 1
+let playBtn = document.querySelector('.play-btn');
+let synthesizer = window.speechSynthesis;
+
+
+playBtn.addEventListener('click', () => {
+    if(inputText.value === ''){
+        alert("Enter something in the text box");
+    }
+    else if(!synthesizer.speaking){
+        speech.text = inputText.value;
+        speech.voice = (voiceSelect.value)?voices[voiceSelect.value]:voices[0]
+        synthesizer.speak(speech);
+        document.querySelector('.play-btn>p').innerHTML = '<i class="fa fa-stop"></i> Stop';
+        speech.onend = () => {
+            document.querySelector('.play-btn>p').innerHTML = '<i class="fa fa-play-circle"></i> Play';
+        }
+    }
+    else if(synthesizer.speaking){
+        synthesizer.cancel();
+        document.querySelector('.play-btn>p').innerHTML = '<i class="fa fa-play-circle"></i> Play';
+    }
+})
+
+let volumeInput = document.querySelector('#volume-input');
+let rateInput = document.querySelector('#rate-input');
+let volumeIndicator = document.querySelector('#volume-indicator');
+let rateIndicator = document.querySelector('#rate-indicator');
+
+
+
+volumeInput.addEventListener('input', () => {
+    if(synthesizer.speaking){
+        synthesizer.cancel();
+        document.querySelector('.play-btn>p').innerHTML = '<i class="fa fa-play-circle"></i> Play';
+    }
+    let volume = volumeInput.value/10;
+    volumeIndicator.innerHTML = volumeInput.value
+    speech.volume = volume
+})
+
+rateInput.addEventListener('input', () => {
+    if(synthesizer.speaking){
+        synthesizer.cancel();
+        document.querySelector('.play-btn>p').innerHTML = '<i class="fa fa-play-circle"></i> Play';
+    }
+    rateIndicator.innerHTML = rateInput.value
+    speech.rate = rateInput.value
+})
+
+let voiceSelect = document.querySelector('select')
+voices = []
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    voices = window.speechSynthesis.getVoices()
+    speech.voice = voices[voiceSelect.value]
+    voices.forEach((voice, index) => (voiceSelect.options[index] = new Option(voice.name, index)));
+}
+
+voiceSelect.addEventListener('input', ()=>{
+    speech.voice = voices[voiceSelect.value]
+})
